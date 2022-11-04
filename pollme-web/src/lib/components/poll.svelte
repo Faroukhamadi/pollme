a2570659353a9511ef52454ffe208c3c2d395447
Faroukhamadi <faroukhamadi1@outlook.com>
2022-11-04 22:24:41 +0100

    add poll component

---
+++ pollme-web/src/lib/components/poll.svelte
@@ -0,0 +1,44 @@
+<div class="flex gap-5 bg-indigo-300 rounded-md m-4 p-4">
+	<div class="flex flex-col justify-center">
+		<button>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				fill="currentColor"
+				aria-hidden="true"
+				class="h-7 w-7 text-neutral-500 hover:rounded-sm hover:bg-neutral-700 hover:bg-neutral-600 hover:text-green-400"
+				><path
+					fill-rule="evenodd"
+					d="M11.47 7.72a.75.75 0 011.06 0l7.5 7.5a.75.75 0 11-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 01-1.06-1.06l7.5-7.5z"
+					clip-rule="evenodd"
+				/></svg
+			>
+		</button>
+		<p class="text-center">0</p>
+		<button>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				fill="currentColor"
+				aria-hidden="true"
+				class="h-7 w-7 text-neutral-500 hover:rounded-sm hover:bg-neutral-700 hover:bg-neutral-600 hover:text-red-400"
+				><path
+					fill-rule="evenodd"
+					d="M12.53 16.28a.75.75 0 01-1.06 0l-7.5-7.5a.75.75 0 011.06-1.06L12 14.69l6.97-6.97a.75.75 0 111.06 1.06l-7.5 7.5z"
+					clip-rule="evenodd"
+				/></svg
+			>
+		</button>
+	</div>
+	<div class="flex flex-col">
+		<h3 class="text-xl">title</h3>
+		<div>
+			<button class="btn btn-sm">thingie1</button>
+			<button class="btn btn-sm">thingie2</button>
+		</div>
+		<div class="flex gap-5 text-sm">
+			<p>1 Votes</p>
+			<p class="text-slate-500">submitted 2 days ago</p>
+		</div>
+	</div>
+</div>
---
+++ pollme-web/src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="navbar bg-base-100">
+	<div class="flex-1">
+		<h1><a class="btn btn-ghost normal-case text-xl" href="/">Pollme</a></h1>
+	</div>
+
+	<div class="navbar-end">
+		<a class="btn" href="/login">Login</a>
+	</div>
+</div>
+<slot />
